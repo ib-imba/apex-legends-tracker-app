794794a6854f6d1f83adab0c7ef39e063486fc7d
ib-imba <imbaxkill@gmail.com>
2019-10-28 10:32:26 -0500

    server

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+
+// Load env
+dotenv.config({ path: './config.env' });
+
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} node on port ${port}`);
+})
