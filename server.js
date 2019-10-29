9d665b0fac9a74c0196859934ccbbed1d2336d2e
ib-imba <imbaxkill@gmail.com>
2019-10-28 21:13:43 -0500

    templates

--- server.js
+++ server.js
@@ -8,16 +8,24 @@ dotenv.config({ path: './config.env' });
 const app = express();
 
 // Dev logging
-if(process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
 // Profile routes
-
 app.use('/api/v1/profile', require('./routes/profile'));
+
+// Handle production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static(__dirname + '/public/'));
+
+  // Handle SPA
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
 
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} node on port ${port}`);
-})
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`);
+});
