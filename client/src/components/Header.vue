0e35045d7810b22374a49a3d828eb29aba142bb2
ib-imba <imbaxkill@gmail.com>
2019-10-28 21:16:28 -0500

    path

---
+++ client/src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <header>
+    <img :src="image" alt="logo">
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data() {
+    return {
+      image: require('@/assets/logo.png')
+    }
+  }
+}
+</script>
+
+<style scoped>
+  img {
+    width: 300px;
+  }
+
+  @media(max-width: 700) {
+    img {
+      display: block;
+      margin: auto;
+    }
+  }
+</style>
